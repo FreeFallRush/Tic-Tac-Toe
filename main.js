c03033124e4ce26fac551821b0dd8f27f144ab94
FreeFallRush <queenofcups.altbot@gmail.com>
2023-03-07 16:56:23 +0200

    Updated main.js
    added functions to allow each player mark empty squares when is their turn and also added checkWinner and showResult

--- main.js
+++ main.js
@@ -92,7 +92,37 @@ const displayController = (() => {
     });
   });
 
+  // let players mark only the empty squares
+  boardContainer.addEventListener("click", (e) => {
+    if (
+      game.getPlay() &&
+      e.target.classList.contains("square") &&
+      e.target.children.length === 0
+    ) {
+      const mark = document.createElement("p");
+      mark.textContent = game.getCurrentPlayer().mark;
+      e.target.appendChild(mark);
+
+      gameBoard.setBoard(e.target.dataset.index, game.getCurrentPlayer().mark);
+      game.nextTurn();
+    }
+  });
+
+  function setComputerTurn(index, mark) {
+    const pMark = document.createElement("p");
+    pMark.textContent = mark;
+    gameBoard.setBoard(index, mark);
+    boardContainer.children[index].appendChild(pMark);
+  }
+
+  function showResult(winner) {
+    document.querySelector("h4").classList.add("win");
+    document.querySelector("h4").textContent = winner;
+  }
+
   return {
+    setComputerTurn,
+    showResult,
     humanPlayerBox,
     computerPlayerBox,
     humanPlayerScore,
@@ -165,11 +195,88 @@ const game = (() => {
     return gameover;
   }
 
+  function nextTurn() {
+    checkWinner();
+
+    if (!gameover) {
+      currentPlayer = currentPlayer === player ? computer : player;
+
+      if (currentPlayer === computer) {
+        computerTurn();
+        nextTurn();
+      }
+    }
+  }
+
+  // set computer to make random move based on empty squares
+  function computerTurn() {
+    const temp = [];
+    gameBoard.getBoard().forEach((element, index) => {
+      if (element === "") {
+        temp.push(index);
+      }
+    });
+    const randPosition = temp[Math.floor(Math.random() * temp.length)];
+    displayController.setComputerTurn(randPosition, currentPlayer.mark);
+  }
+
+  // Checking for winners based on winning combinations
+  function checkWinner() {
+    let isWon = false;
+    const board = gameBoard.getBoard();
+    const win = gameBoard.getWinningCombos();
+    const squares = document.querySelectorAll("[data-index]");
+
+    for (let i = 0; i < win.length; i++) {
+      const condition = win[i];
+      // checking if the marked boxes are in a row for a win
+      const box1 = board[condition[0]];
+      const box2 = board[condition[1]];
+      const box3 = board[condition[2]];
+      if (box1 === "" || box2 === "" || box3 === "") {
+        continue;
+      }
+      if (box1 === box2 && box2 === box3) {
+        const winner =
+          board[condition[0]] === player.mark ? "You win!" : "Computer wins!";
+
+        // adding score based on winner
+        if (winner === "You win!") {
+          addHumanScore();
+          displayController.humanPlayerScore.textContent = `Your Score: ${getHumanScore()}`;
+        } else {
+          addComputerScore();
+          displayController.computerPlayerScore.textContent = `Bot Score: ${getComputerScore()}`;
+        }
+        squares[condition[0]].classList.add("winner");
+        squares[condition[1]].classList.add("winner");
+        squares[condition[2]].classList.add("winner");
+        for (i = 0; i < squares.length; i++) {
+          squares[i].style.pointerEvents = "none";
+        }
+
+        displayController.showResult(winner);
+        isWon = true;
+        gameover = true;
+        return winner;
+      }
+    }
+
+    if (!board.includes("") && !isWon) {
+      displayController.showResult("It's a tie!");
+      squares.forEach((square) => square.classList.add("draw"));
+      gameover = true;
+    } else {
+      play = !gameover;
+    }
+  }
+
   return {
     resetGame,
     setPlayers,
     getCurrentPlayer,
     getPlay,
     isGameOver,
+    nextTurn,
   };
 })();
